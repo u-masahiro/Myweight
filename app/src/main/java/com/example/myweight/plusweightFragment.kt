7f1b5248be8d8e723f87a5aac5a8743862b5e972
u-masahiro <ume61090620@gmail.com>
2020-05-08 22:59:49 +0900

    Revert "アプリ内容をサイトを見て実装"
    This reverts commit 11f4c72044097c6c0133e7f809339071baad0d4e.

--- app/src/main/java/com/example/myweight/plusweightFragment.kt
+++ app/src/main/java/com/example/myweight/plusweightFragment.kt
@@ -2,38 +2,16 @@ package com.example.myweight
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.content.Context
-import android.content.SharedPreferences
-import kotlinx.android.synthetic.main.fragment_plusweight.*
-
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 class plusweightFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // "DataStore"という名前でインスタンスを生成
-        val dataStore: SharedPreferences = getSharedPreferences("DataStore", Context.MODE_PRIVATE)
-
-        buttonWrite.setOnClickListener{
-            // エディットテキストのテキストを取得
-            val stringText = editText.text.toString()
-
-            textWrite.text = stringText
-
-            // 入力文字列を"Input"に書き込む
-            val editor = dataStore.edit()
-            editor.putString("Input", stringText)
-
-            //editor.commit();
-            editor.apply()
-        }
-
-        buttonRead.setOnClickListener{
-            // "Input"から読み出す
-            val str = dataStore.getString("Input", "NoData")
-
-            textRead.text = str
-
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_plusweight,container,false)
     }
-
 }
